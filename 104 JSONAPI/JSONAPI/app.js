39fb0243b199654fdc5fb0fa7bc278999273506b
Pedro <hernansaldana52@gmail.com>
2020-06-10 18:02:59 -0500

    Tercer proyecto de JSON y rest api con ajax

---
+++ 104 JSONAPI/JSONAPI/app.js
@@ -0,0 +1,29 @@
+const cargarPosts = document.querySelector('#cargar').addEventListener('click', cargarAPI);
+
+
+function cargarAPI(){
+    //crear el objeto
+    const xhr = new XMLHttpRequest();
+    //abrir la conexion
+    xhr.open('GET','https://jsonplaceholder.typicode.com/posts', true);
+    //Carga y lee los datos
+    xhr.onload = function() {
+    if(this.status === 200){
+        const respuesta = JSON.parse (this.responseText);
+        
+        let contenido = '';
+        respuesta.forEach(function(post){
+            contenido += ` 
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+            
+            `;
+        });
+        document.getElementById('listado').innerHTML = contenido;
+        }
+    }
+    
+    // enviar la conexion
+    xhr.send();
+}
+
