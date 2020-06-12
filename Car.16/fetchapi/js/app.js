e0e883e53d1eec0385c58f5fad7a4c17f8c8a5f8
Pedro <hernansaldana52@gmail.com>
2020-06-11 21:10:35 -0500

    fetch API con datos funcionando desde un archivo JSON

--- Car.16/fetchapi/js/app.js
+++ Car.16/fetchapi/js/app.js
@@ -1,4 +1,6 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+
 
 
 function cargarTXT(){
@@ -10,6 +12,26 @@ function cargarTXT(){
         .then(function(empleados){
             console.log(empleados);
             document.getElementById('resultado').innerHTML = empleados;
+        })
+        .catch(function(error){
+            console.log(error);
+        });
+}
+
+
+function cargarJSON() {
+    fetch('empleados.json')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            let html = '';
+            data.forEach(function(empleado){
+                html +=` 
+                    <li>${empleado.nombre} ${empleado.puesto}</li>
+                `;
+            })
+            document.getElementById('resultado').innerHTML = html;
         })
         .catch(function(error){
             console.log(error);
