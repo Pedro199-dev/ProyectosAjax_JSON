f5d16c62cd86b5d55feee5433db2f2efc1ff6128
Pedro <hernansaldana52@gmail.com>
2020-06-11 19:01:01 -0500

    Callbacks en javascript

---
+++ Car.15/109 Callbacks/app.js
@@ -0,0 +1,52 @@
+//Listado de paises
+const paises = ['Francia', 'España', 'Portugal', 'Australia', 'Inglaterra', 'Irlanda'];
+
+// Se agrega un nuevo pais despues de 2 segundos
+function nuevoPais(pais, callback){
+    setTimeout(function(){
+        paises.push(pais);
+        callback();
+
+    }, 2000);
+}
+
+//Los paises se muestran despues de 1 segundo
+function mostrarPaises() {
+    setTimeout(function() {
+        let html = '';
+        paises.forEach(function(pais) {
+            html += `<li>${pais}</li>`;
+        });
+        document.getElementById('app').innerHTML = html;
+    }, 1000);
+}
+
+// Agregar nuevo pais 
+nuevoPais('Alemania', mostrarPaises);
+
+
+// Mostrar los paises
+mostrarPaises();
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// Callback en Foreach
+
+const cuidades = ['Londres', 'New York', 'Madrid', 'Paris', 'Viena'];
+
+//Inline Callback
+cuidades.forEach(function(cuidad){
+    console.log(cuidad);
+})*/
+
+
